aa9cd1bf37bbbb74e9ee02aeb1203b3b42e904ce
Nigless <nigless>
2021-01-14 14:21:09 +0000

    Added global styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import App from './components/app';
 
+const Styles = createGlobalStyle({
+	body: {
+		background: '#ddd',
+	},
+});
+
 const mountNode = document.getElementById('app');
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+	<>
+		<Styles />
+		<App />
+	</>,
+	mountNode
+);
