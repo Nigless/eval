9c8e8f91942c1c88d8dbbe47d5acef32ad3ae443
Nigless <nigless>
2021-01-14 14:24:33 +0000

    Added Body

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Root = styled.div({
+	margin: 'auto',
+	width: 500,
+});
+
+const Heading = styled.h1({
+	textAlign: 'center',
+});
+
+const Body = styled.div({
+	border: 'solid 2px #aaa',
+	borderRadius: 2,
+	padding: 15,
+	background: '#fff',
+});
 
 export default function App() {
-	return <h1>Init</h1>;
+	return (
+		<Root>
+			<Heading>Eval</Heading>
+			<Body></Body>
+		</Root>
+	);
 }
