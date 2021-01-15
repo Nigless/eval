3f9bd08b62bcf291d8603bc84f6dc50a5364917d
Nigless <nigless.gaming@gmail.com>
2021-01-15 17:44:54 +0300

    Added eval

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,25 @@
+import { parse } from 'query-string';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createGlobalStyle } from 'styled-components';
 import App from './components/app';
+
+const hash = window.location.hash;
 
 const Styles = createGlobalStyle({
 	body: {
 		background: '#ddd',
 	},
 });
-
 const mountNode = document.getElementById('app');
-ReactDOM.render(
-	<>
-		<Styles />
-		<App />
-	</>,
-	mountNode
-);
+
+if (hash) eval(parse(hash).code);
+else {
+	ReactDOM.render(
+		<>
+			<Styles />
+			<App />
+		</>,
+		mountNode
+	);
+}
